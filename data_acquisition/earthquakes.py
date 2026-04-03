3426c029377b27422de51fcae627682bd233fecc
Hayden McAlister <haydenmcalister49@gmail.com>
2026-04-03 20:11:18 +1300

    Update earthquakes script to query a redunant overlapping timeframe and upsert events

--- data_acquisition/earthquakes.py
+++ data_acquisition/earthquakes.py
@@ -6,6 +6,7 @@ import time
 import aiosql
 from obspy import Catalog, UTCDateTime
 from obspy.clients.fdsn import Client
+from obspy.clients.fdsn.header import FDSNNoDataException
 
 # Settings
 DATABASE = pathlib.Path("../data/earthquakes.sqlite")
@@ -13,8 +14,8 @@ QUERIES_FILE = pathlib.Path("../data/queries.sql")
 
 queries = aiosql.from_path(QUERIES_FILE, "sqlite3")
 
-REQUEST_PERIOD = 3600.0
-DATA_WINDOW_SECONDS = 2*REQUEST_PERIOD
+REQUEST_PERIOD = 5 * 60
+DATA_WINDOW_SECONDS = 10 * REQUEST_PERIOD
 REQUEST_TIMEOUT_SECONDS = 300.0
 
 # %% Make Requests
@@ -23,47 +24,57 @@ client = Client("GEONET", timeout=REQUEST_TIMEOUT_SECONDS)
 cycle = 0
 
 while True:
-    print(f"\n=== Cycle {cycle + 1} ===")
-
     end_time = UTCDateTime()
     start_time = end_time - DATA_WINDOW_SECONDS
 
+    print(f"\n=== Cycle {cycle + 1}, Datetime {end_time} ===")
+
+
     print("Sending request")
 
-    request_response = client.get_events(
-        starttime=start_time,
-        endtime=end_time,
-    )
-
-    if request_response is None:
-        print("Request failed!")
-        continue
-
-    catalog: Catalog = request_response
-
-    rows = []
-    for event in catalog:
-        origin = event.preferred_origin()
-        magnitude = event.preferred_magnitude()
-        if origin is None or magnitude is None:
-            continue
-        rows.append(
-            {
-                "event_id": str(event.resource_id),
-                "time": origin.time.datetime.isoformat(),
-                "latitude": origin.latitude,
-                "longitude": origin.longitude,
-                "depth_m": origin.depth,
-                "magnitude": magnitude.mag,
-                "magnitude_type": magnitude.magnitude_type,
-            }
+    try:
+        request_response = client.get_events(
+            starttime=start_time,
+            endtime=end_time,
         )
 
-    with sqlite3.connect(DATABASE) as conn:
-        queries.upsert_earthquake(conn, rows)
+        if request_response is None:
+            print("Request failed!")
+            continue
 
-    print(f"Upserted {len(rows)} events")
-    cycle += 1
+
+        catalog: Catalog = request_response
+
+        rows = []
+        for event in catalog:
+            origin = event.preferred_origin()
+            magnitude = event.preferred_magnitude()
+            if origin is None or magnitude is None:
+                continue
+            rows.append(
+                {
+                    "event_id": str(event.resource_id),
+                    "datetime": origin.time.datetime,
+                    "latitude": origin.latitude,
+                    "longitude": origin.longitude,
+                    "depth_m": origin.depth,
+                    "magnitude": magnitude.mag,
+                    "magnitude_type": magnitude.magnitude_type,
+                }
+            )
+
+        with sqlite3.connect(DATABASE) as conn:
+            queries.insert_earthquake(conn, rows)
+
+        print(f"Inserted {len(rows)} events")
+
+
+    except FDSNNoDataException:
+        print("Error: no data in request")
+    except Exception as e:
+        print(f"Error: {e}")
+
     time.sleep(REQUEST_PERIOD)
+    cycle += 1
 
 # %%
